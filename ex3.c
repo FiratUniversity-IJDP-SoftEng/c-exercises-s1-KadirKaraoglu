7703bc53441c7b058cd642eb3d7885d1a4015652
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2024-10-22 23:08:33 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main(void){
+char str[3] ;
+printf("Do you have a cat or dog?\n");
+scanf("%s",str);
+if(strcmp(str , "cat") ==0){
+    printf("meow\n");
+
+}
+ else if(strcmp(str , "dog") ==0){
+    printf("woofff\n");
+}
+  else {
+    printf("I didnt understant what you wrote,please try again\n");
+  }
+}
 

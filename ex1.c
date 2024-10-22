2f6a86795d8069c842a09aeea472278cc635eea3
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2024-10-22 22:27:07 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,20 @@
-gfhdfth gfhdfghgdbndfghfgnbgfh
+#include<stdio.h>
+
+
+int main(void){
+int n;
+printf("I chose a namber between one and five\nLets see if you can guess it??\n");
+
+scanf("%d",&n);
+if(n==3){
+printf("congratulations, you guessed it right");
+}
+else if (1<=n && n<6 && n!=3){
+printf("sorry, you didnt guess corretly");
+}
+else if( 1>n || n>5)
+{
+printf("nooo broo ,you have to enter number between 1 and 5");
+}
+
+}

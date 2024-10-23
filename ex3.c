e03fb565e7238c9ded6a2973087bae53250a5ea1
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2024-10-23 09:39:30 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main(void){
 char str[3] ;
-printf("Do you have a cat or dog?\n");
+printf("Do you have a 'cat' or 'dog'?\n");
+  do{
 scanf("%s",str);
 if(strcmp(str , "cat") ==0){
     printf("meow\n");
+  break;
 
 }
  else if(strcmp(str , "dog") ==0){
     printf("woofff\n");
+   break;
 }
   else {
     printf("I didnt understant what you wrote,please try again\n");
   }
-}
-
+  }while(strcmp(str , "cat") !=0 && strcmp , (str , "dog" !=0));
+  }
